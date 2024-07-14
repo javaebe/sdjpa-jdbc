91e6513f6e98b9044472f28509c5906835611758
ebe <ebephp@gmail.com>
2024-07-14 22:45:37 +0530

    author dao using JDBC template

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -1,169 +1,59 @@
 package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
-    public AuthorDaoImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Author saveAuthor(Author author) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
+        jdbcTemplate.update("INSERT INTO author (first_name,last_name) VALUES(?,?)",
+                author.getFirstName(), author.getLastName());
 
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("INSERT INTO author (first_name,last_name) VALUES(?,?)");
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.execute();
+        Long id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
 
-            Statement statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT LAST_INSERT_ID()");
-
-            if (resultSet.next()) {
-                return getById(resultSet.getLong(1));
-            }
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        if (id != null) {
+            return getById(id);
         }
         return null;
     }
 
     @Override
     public Author getById(Long id) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?");
-            ps.setLong(1, id);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromRS(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM author WHERE id = ?", getRowMapper(), id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("SELECT * FROM author WHERE first_name = ? AND last_name = ?");
-            ps.setString(1, firstName);
-            ps.setString(2, lastName);
-            resultSet = ps.executeQuery();
-
-            if (resultSet.next()) {
-                return getAuthorFromRS(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM author WHERE first_name = ? AND last_name = ?", getRowMapper(),
+                firstName, lastName);
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("UPDATE author SET first_name=?,last_name=? WHERE id=?");
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setLong(3, author.getId());
-            ps.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        jdbcTemplate.update("UPDATE author SET first_name=?,last_name=? WHERE id=?",
+                author.getFirstName(),
+                author.getLastName(),
+                author.getId());
         return getById(author.getId());
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-        Connection connection = null;
-        PreparedStatement ps = null;
-
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement("DELETE FROM author WHERE id=?");
-            ps.setLong(1, id);
-            ps.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(null, ps, connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        jdbcTemplate.update("DELETE FROM author WHERE id=?", id);
     }
 
-    private Author getAuthorFromRS(ResultSet resultSet) throws SQLException {
-        Author author = new Author();
-        author.setId(resultSet.getLong("id"));
-        author.setFirstName(resultSet.getString("first_name"));
-        author.setLastName(resultSet.getString("last_name"));
-        return author;
-    }
-
-    private void closeAll(ResultSet resultSet, PreparedStatement ps, Connection connection) throws SQLException {
-        if (resultSet != null) {
-            resultSet.close();
-        }
-        if (ps != null) {
-            ps.close();
-        }
-        if (connection != null) {
-            connection.close();
-        }
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
     }
 }
--- src/test/java/guru/springframework/jdbc/AuthorDaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/AuthorDaoIntegrationTest.java
@@ -1,27 +1,31 @@
 package guru.springframework.jdbc;
 
 import guru.springframework.jdbc.dao.AuthorDao;
-import guru.springframework.jdbc.dao.AuthorDaoImpl;
+import guru.springframework.jdbc.dao.BookDao;
 import guru.springframework.jdbc.domain.Author;
+import guru.springframework.jdbc.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("local")
 @DataJpaTest
-//@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
-@Import(AuthorDaoImpl.class)
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AuthorDaoIntegrationTest {
 
     @Autowired
     AuthorDao authorDao;
+
+    @Autowired
+    BookDao bookDao;
 
     @Test
     void updateAuthor() {
@@ -45,7 +49,7 @@ class AuthorDaoIntegrationTest {
     }
 
     @Test
-    void getById() {
+    void getAuthorById() {
         Author author = authorDao.getById(1L);
 
         assertThat(author).isNotNull();
@@ -65,10 +69,73 @@ class AuthorDaoIntegrationTest {
         author.setLastName("Doe");
         Author saved = authorDao.saveAuthor(author);
 
-        authorDao.deleteAuthorById(saved.getId());
+        Long id = saved.getId();
+        authorDao.deleteAuthorById(id);
 
-        Author deleted = authorDao.getById(saved.getId());
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            authorDao.getById(id);
+        });
+    }
 
-        assertThat(deleted).isNull();
+    @Test
+    void updateBook() {
+        Book book = new Book();
+        book.setTitle("M Book");
+        book.setIsbn("132");
+
+        Author author = new Author();
+        author.setId(1L);
+
+        book.setAuthor(author);
+        book.setPublisher("I am");
+        Book saved = bookDao.saveBook(book);
+
+        saved.setIsbn("321");
+        Book updated = bookDao.updateBook(saved);
+        assertThat(updated.getIsbn()).isEqualTo("321");
+    }
+
+    @Test
+    void saveBook() {
+        Book book = new Book();
+        book.setTitle("M Book");
+        book.setIsbn("132");
+
+        Author author = new Author();
+        author.setId(1L);
+
+        book.setAuthor(author);
+        book.setPublisher("I am");
+        Book saved = bookDao.saveBook(book);
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    void getBookById() {
+        Book book = bookDao.getById(1L);
+
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void findBookByName() {
+        Book book = bookDao.findBookByTitle("M Book");
+
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void deleteBookById() {
+        Book book = new Book();
+        book.setTitle("M Book");
+        book.setIsbn("132");
+        book.setPublisher("I am");
+        Book saved = bookDao.saveBook(book);
+
+        Long id = saved.getId();
+        bookDao.deleteBookById(id);
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            bookDao.getById(id);
+        });
     }
 }
